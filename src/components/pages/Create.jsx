761a0ac09c495cd88c4929297701f6cd290dd53a
bautiliendo <juanbautistaliendo1@gmail.com>
2024-04-29 14:38:01 -0300

    avances 29/4

--- src/components/pages/Create.jsx
+++ src/components/pages/Create.jsx
@@ -1,68 +1,66 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
-import {useState} from "react";
+import { useState } from "react";
 import { useForm } from "../../hooks/useForm";
 import { Peticion } from "../../helpers/Peticion";
 import { Global } from "../../helpers/Global";
 
 export const Create = () => {
-
-  const {form, sent , changed} = useForm({});
-  const [result, setResult] = useState("notSend")
+  const { form, sent, changed } = useForm({});
+  const [result, setResult] = useState("notSend");
 
   const saveArticle = async (e) => {
     e.preventDefault();
-
     //Recoger datos form
     let newArticle = form;
-
     //Guardar article en el backend
-    const {data} = await Peticion(Global.url+"create", "POST", newArticle);
-    console.log(data)
+    const { data } = await Peticion(Global.url + "create", "POST", newArticle);
     if (data.status === "success") {
-      setResult("saved")
-      //Subir img
-      const fileInput = document.querySelector("#file");
-
+      setResult("saved");
+    } else {
+      setResult("error");
+    }
+    //Subir img
+    const fileInput = document.querySelector("#file");
+    if (data.status === "success" && fileInput.files[0]) {
+      setResult("saved");
+  
       const formData = new FormData();
-      formData.append('file0', fileInput.files[0]);
+      formData.append("file0", fileInput.files[0]);
 
-      const {upload} = await Peticion(Global.url+"upload-img/"+data.article._id, "POST", formData, true);
-      console.log(upload)
-      if(upload.status === "success"){
-        setResult("saved")
-      } else{
+      //const subida
+      const uploadResponse = await Peticion(Global.url + "upload-img/" + data.article._id,"POST",formData,true);
+      if (uploadResponse.data.status === "success") {
+        setResult("saved");
+      } else {
         setResult("error");
       }
-    } 
-    else {
-      setResult("error")
-
+      console.log(uploadResponse) 
     }
-    console.log(data)
-  }
-  
-
+  };
 
   return (
     <div className="jumbo">
       <h1>Crear articulo</h1>
       <p>Formulario para crear articulo</p>
-      
-      <strong>{result == "saved" ? "Articulo guardado con exito !!" : ""}</strong>
-      <strong>{result == "error" ? "Los datos proporcionados son incorrectos" : ""}</strong>
+
+      <strong>
+        {result == "saved" ? "Articulo guardado con exito !!" : ""}
+      </strong>
+      <strong>
+        {result == "error" ? "Los datos proporcionados son incorrectos" : ""}
+      </strong>
 
       {/*Montar form */}
       <form className="formulario" onSubmit={saveArticle}>
-
         <div className="form-group">
           <label>Titulo</label>
-          <input type="text" name="title" onChange={changed}/>
+          <input type="text" name="title" onChange={changed} />
         </div>
 
         <div className="form-group">
           <label>Contenido</label>
-          <input type="text" name="content" onChange={changed}/>
+          <input type="text" name="content" onChange={changed} />
         </div>
 
         <div className="form-group">
--- src/components/pages/List.jsx
+++ src/components/pages/List.jsx
@@ -1,4 +1,6 @@
+// eslint-disable-next-line no-unused-vars
 import React from "react";
+import { Global } from "../../helpers/Global";
 
 export const List = ({articles, setArticles}) => {
   return (
@@ -6,7 +8,8 @@ export const List = ({articles, setArticles}) => {
         return (
           <article key={article._id} className="articulo-item">
             <div className="mascara">
-              <img src="https://imgs.search.brave.com/nQFdkbBtRdnUMRLD2d-iV4y7HK1t3zxB6np9jevVV2Y/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9kZXNh/cnJvbGxvd2ViLmNv/bS9zdG9yYWdlL3Rh/Z19pbWFnZXMvYWN0/dWFsLzNCcFVEQnND/M29mQnk0dG9yVDFa/aVVGNmhkb25xcFJI/ODQxTWVNRE0ucG5n" />
+            {article.image && article.image !== "default.png" && <img src={Global.url + "image/" + article.image} alt={article.title}/>}
+            {!article.image && <img src="https://imgs.search.brave.com/nQFdkbBtRdnUMRLD2d-iV4y7HK1t3zxB6np9jevVV2Y/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9kZXNh/cnJvbGxvd2ViLmNv/bS9zdG9yYWdlL3Rh/Z19pbWFnZXMvYWN0/dWFsLzNCcFVEQnND/M29mQnk0dG9yVDFa/aVVGNmhkb25xcFJI/ODQxTWVNRE0ucG5n" alt="Default Image" />}
             </div>
             <div className="datos"></div>
             <h3 className="title">{article.title}</h3>
